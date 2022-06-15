f686b7ae3ee410148a00a9bc17c00801b7ea4b8b
rafael mendes <rafael.mendes@tallos.com.br>
2022-06-15 14:16:39 -0300

    começando a configurar sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LivrosController } from './livros.controller';
 import { LivrosService } from './livros.services';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      models: [],
+    }),
+  ],
   controllers: [AppController, LivrosController],
   providers: [AppService, LivrosService],
 })
